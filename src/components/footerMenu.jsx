f20372a3a18cc3230f9f4a3eb3b6c0564b1c2285
alisonnRB <alisonrbjs@gmail.com>
2024-07-30 12:57:31 -0300

    router

--- src/components/footerMenu.jsx
+++ src/components/footerMenu.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import categories from "../assets/categories.png";
 import cart from "../assets/cart.png";
@@ -8,20 +9,27 @@ export default function Menu() {
     return (
         <footer className="w-full h-[10dvh] mt-[20dvh] z-20 bg-subMotion flex justify-center items-center gap-[20%] self-center">
 
-            <Image 
-                src={categories}
-                className="h-[50%] object-contain cursor-pointer"
-            />
-            
-            <Image 
-                src={cart}
-                className="h-[130%] object-contain relative bottom-[40%] cursor-pointer"
-            />
-            
-            <Image 
-                src={profile}
-                className="h-[50%] w-max-[50%] object-contain cursor-pointer"
-            />
+            <Link href={'/cart'} className="h-full flex justify-center items-center">
+                <Image
+                    src={categories}
+                    className="h-[50%] object-contain cursor-pointer"
+                />
+            </Link>
+
+            <Link href={'/cart'} className="h-full">
+
+                <Image
+                    src={cart}
+                    className="h-[130%] object-contain relative bottom-[40%] cursor-pointer"
+                />
+            </Link>
+
+            <Link href={'/cart'} className="h-full flex justify-center items-center">
+                <Image
+                    src={profile}
+                    className="h-[50%] w-max-[50%] object-contain cursor-pointer"
+                />
+            </Link>
         </footer>
     );
 }
