3a8bd46a54b9ec6bf45cbf6fd55fc42d1fc5bf18
alisonnRB <alisonrbjs@gmail.com>
2024-08-02 15:51:52 -0300

    rigth

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,31 @@ import { CartProvider } from "@/script/cartContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Catche beer",
-  description: "Delivery de bebidas em são paulo - SP, o mias rápido da região, peça rapidamente pelo nosso website com uma grande variedade de bebidas para você aproveitar o seu dia",
+  title: "Catche Beer - Delivery de Bebidas em SP",
+  description: "Delivery de bebidas em São Paulo - SP, o mais rápido da região. Peça rapidamente pelo nosso website com uma grande variedade de bebidas para você aproveitar o seu dia.",
+  keywords: "delivery, bebidas, São Paulo, cerveja, vinho, refrigerantes, destilados",
+  openGraph: {
+    title: "Catche Beer - Delivery de Bebidas",
+    description: "Descubra a melhor forma de receber bebidas em São Paulo com Catche Beer.",
+    url: "https://www.catchebeer.com",
+    // images: [
+    //   {
+    //     url: "https://www.catchebeer.com/og-image.jpg",
+    //     width: 800,
+    //     height: 600,
+    //     alt: "Catche Beer Logo",
+    //   },
+    // ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <CartProvider>
         <body className={inter.className}>
           {children}
